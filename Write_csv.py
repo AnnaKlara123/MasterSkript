fab54361c348d2a9e7ffb4583de02d7c6f6a654f
Anna Stang <anna.stang@student.uibk.ac.at>
2023-01-02 15:26:55 +0100

    get date & Time

--- Write_csv.py
+++ Write_csv.py
@@ -5,7 +5,7 @@ import csv
 # csv file name
 filename = "C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/Daten/Meteorologie/jamtalhütte_20200401/GS_test.csv"
 
- # initializing the titles and rows list
+# initializing the titles and rows list
 fields = []
 rows = []
 yy = []
@@ -25,8 +25,8 @@ with open(filename, 'r') as csvfile:
     for row in csvreader:
         rows.append(row) # Raws werden der vorher erstellten Liste rows = [ ] nacheinander hinzugefügt
 
-    #get total number of rows
-    #print("Total no. of rows: %d"%(csvreader.line_num))
+                        #get total number of rows
+                        #print("Total no. of rows: %d"%(csvreader.line_num))
  
                         #### printing the field names
                         # print('Field names are:' + ', '.join(field for field in fields))
@@ -35,7 +35,7 @@ with open(filename, 'r') as csvfile:
                         # print('\nFirst 100 rows are:\n') # \n benutzt man, um zur nächsten Zeile wechseln
                         # # print(rows[:100]) # gibt die die Werte ohne Zeilenabsatz aus!
 
-######## get date & time ###########
+######## get Date & Time ###########
 
 for row in rows[:100]: 
     # parsing each column of a row
@@ -47,8 +47,10 @@ for row in rows[:100]:
         yy= (col[:4])
         mm = (col[4:6])
         dd = (col[6:8])
-        hh = (col[8:12])
-        print('yy=', yy,'mm=',mm,'dd=',dd, 'hh=',hh)
+        hh = (col[8:12]) # WICHTIG: Hier nochmal genau schauen, wie ich das in Min umrechne!
+        
+        # print('yy=', yy,'mm=',mm,'dd=',dd, 'hh=',hh)
+
 
 
 # ########### CSV WRITE ################
