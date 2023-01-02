89ca267758b80d4196ca585478915d0f319bf6d5
Anna Stang <anna.stang@student.uibk.ac.at>
2023-01-02 13:27:47 +0100

    erstes output, Trenner und Zeilen müssen noch angepasst werden

--- Write_csv.py
+++ Write_csv.py
@@ -21,48 +21,52 @@ with open(filename, 'r') as csvfile:
     for row in csvreader:
         rows.append(row) # Raws werden der vorher erstellten Liste rows = [ ] nacheinander hinzugefügt
  
-    # get total number of rows
-    # print("Total no. of rows: %d"%(csvreader.line_num))
+    #get total number of rows
+    #print("Total no. of rows: %d"%(csvreader.line_num))
+#print(rows[:10])
+ 
+# #### printing the field names
+# # print('Field names are:' + ', '.join(field for field in fields))
+ 
+# # printing first 5 rows
+# # print('\nFirst 100 rows are:\n') # \n benutzt man, um zur nächsten Zeile wechseln
+# # # print(rows[:100]) # gibt die die Werte ohne Zeilenabsatz aus!
+# for row in rows[:100]: 
+#     # parsing each column of a row
+#     for col in row:
+#         # print(col)  # Hier wäre col und row das Selbe nur anders "verpackt"? WHY? 
+#         # print(row)
+#         # print("%10s"%col), Gibt die Liste ohne ' ' und [] aus
+#     #print('\n') # Sorgt nur für Leerzeile 
 
- 
-# printing the field names
-# print('Field names are:' + ', '.join(field for field in fields))
- 
-# printing first 5 rows
-# print('\nFirst 100 rows are:\n') # \n benutzt man, um zur nächsten Zeile wechseln
-# # print(rows[:100]) # gibt die die Werte ohne Zeilenabsatz aus!
-for row in rows[:100]: 
-    # parsing each column of a row
-    for col in row:
-        print(col)  # Hier wäre col und row das Selbe nur anders "verpackt"? WHY? 
-        #print("row are:", row)
-        # print("%10s"%col), Gibt die Liste ohne ' ' und [] aus
-    #print('\n') # Sorgt nur für Leerzeile 
 
-# ########### CSV WRITE ################
+########### CSV WRITE ################
 
-#     # field names
-# fieldsout = ['YY',	'MM',	'DD',	'HH',	'Stat1']
+    # field names
+fieldsout = ['YY',	'MM',	'DD',	'HH',	'Stat1']
  
-# # data rows of csv file
-# rowsout = [ rows]
+# data rows of csv file
+rowsout = [ rows]
+
+# name of csv file
+csvOut = "csvOutputGS.csv"
  
  
-# # writing to csv file
-# with open(filename, 'w') as csvfile:
-#     # creating a csv writer object
-#     csvwriter = csv.writer(csvfile)
+# writing to csv file
+with open(csvOut, 'w') as csvfile:
+    # creating a csv writer object
+    csvwriter = csv.writer(csvfile)
      
-#     # writing the fields
-#     csvwriter.writerow(fieldsout)
+    # writing the fields
+    csvwriter.writerow(fieldsout)
      
-#     # writing the data rows
-#     csvwriter.writerows(rowsout)
+    # writing the data rows
+    csvwriter.writerows(rowsout)
 
 
-# #csvwriter.to_csv(r'"C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/Daten/Meteorologie/jamtalhütte_20200401/export_GS_test.csv', index=False, header=True)
+#csvwriter.to_csv(r'"C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/Daten/Meteorologie/jamtalhütte_20200401/export_GS_test.csv', index=False, header=True)
 
-# print(csvwriter)
+print(csvwriter)
 
 
 
