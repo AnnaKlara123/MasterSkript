301ef98b3f128ddb14e275e112f007a53eecdf55
Anna Stang <anna.stang@student.uibk.ac.at>
2023-01-08 11:13:20 +0100

    Clean skript

--- AverageData.py
+++ AverageData.py
@@ -1,13 +1,14 @@
-# my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+###### Import for Python #########
+
 import csv
 import pandas as pd
 import numpy as np
 
-# csv file name
+####### csv file name  #####
 filename = "C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/Daten/Meteorologie/jamtalhütte_20200401/GS_test2.csv"
 ###### Hier später über alle Files in einem Ordner iterieren lassen! ####### --> Siehe Python Kurs 
 
-# initializing the titles and rows list
+###### initializing the titles and rows list #####
 fields = []
 rows = []
 yy = []
@@ -16,8 +17,9 @@ dd = []
 hh= []
 Stat1 = []
  
-# reading csv file
-with open(filename, 'r') as csvfile:
+###### reading csv file ########
+
+with open(filename, 'r') as csvfile:                            # file name vorher bestimmmt
     # creating a csv reader object
     csvreader = csv.reader(csvfile, delimiter = " ")
     for row in csvreader:
@@ -37,23 +39,30 @@ for row in rows[:300]:
         hh.append(col[8:12]) # WICHTIG: Hier nochmal genau schauen, wie ich das in Min umrechne!
         Stat1.append(col[15:])
 
+
+####### Aus Str. Int machen #######
 yy = [int(x) for x in yy]
 mm = [int(x) for x in mm]
 dd = [int(x) for x in dd]
 hh= [int(x) for x in hh]
 Stat1 = [float(x) for x in Stat1]
  
-# print("type of dd element is:", type(Stat1[1])) ---> Elemente werden als String eingelesen
-# yyArray = np.array(yy, dtype=int)
-# Stat1Array = np.array(Stat1, dtype=float)
+                # print("type of dd element is:", type(Stat1[1])) ---> Elemente werden als String eingelesen
+                # yyArray = np.array(yy, dtype=int)
+                # Stat1Array = np.array(Stat1, dtype=float)
 
+
+######## Function for Average  #########
 def Average(lst):
     return sum(lst) / len(lst) 
+
+            ####### Steps for calulation ######
 
 step = 10
 sub_lists = []
 averages = []
 
+##### Unterlisten für 10er Steps erstellen ########
 for i, _ in enumerate(Stat1[::step]):               # Sagt es soll von 0-9 über den Code laufen
     sub_list = Stat1[i*10:] if (i+1)*10 > len(Stat1) else Stat1[i*10:(i+1)*10]  # Condition if the len(my_list) % step != 0
     sub_lists.append(sub_list)
