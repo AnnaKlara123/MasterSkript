fd6721b7acd63dd15e5327d249e3797994ee0075
Anna Stang <anna.stang@student.uibk.ac.at>
2023-01-11 14:20:09 +0100

    Erklärung hinzufügen

--- IterateOverFiles.py
+++ IterateOverFiles.py
@@ -1,6 +1,11 @@
-import pandas as pd
 import glob
 import os, sys
+
+##################### Skript um Datentyp zu verändern.################################
+############## um dies zu tun den Pfard in Zeile 7 bez. 23 ändern. auf / vs. \ achten beim Kopieren aus Explorer! 
+############## Jeweils gewünschten daten-Typ anpassen in Zeile 12 bzw. 29 
+
+############## Schauen, welche .csv datein schon vorhanden sind und Directory Liste erstellen #############
 
 directory = os.fsencode('C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/Daten/Meteorologie/Jamtalhütte_20210824_20221004')
     
@@ -13,17 +18,16 @@ for file in os.listdir(directory):
      else:
          continue
 print(directory) 
-        ######## Wie mache ich von hier aus weiter, dass ich nacheinander die Datein abarbeite? ##################### 
 
-# for file in glob.iglob(os.path.join('C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/Daten/Meteorologie/Jamtalhuette_20131010_20221128', '*.zrx')):
-#     os.rename(filename, filename[:-4] + '.csv')
+
+######## Datentyp gewünscht verändern, Iteration über ganzen Ordner ##################### 
 
 folder = 'C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/Daten/Meteorologie/Jamtalhütte_20210824_20221004'
 for filename in os.listdir(folder):
     infilename = os.path.join(folder,filename)
     if not os.path.isfile(infilename): continue
     oldbase = os.path.splitext(filename)
-    newname = infilename.replace('.zrx', '.csv')
+    newname = infilename.replace('.zrx', '.csv')                    # WICHTIG: Wenn es .csv Datei schon in Ordner gibt läuft das Skript nicht!
     output = os.rename(infilename, newname)
 
 print("Output:",output)
