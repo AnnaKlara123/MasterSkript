bc1e3d8513f7e09587a32789dd65e3c86621e3da
Anna Stang <anna.stang@student.uibk.ac.at>
2023-01-11 14:13:42 +0100

    Code to change datatype of all files in dictonary

--- IterateOverFiles.py
+++ IterateOverFiles.py
@@ -1,46 +1,32 @@
-import os
 import pandas as pd
 import glob
-import os
+import os, sys
 
-# directory = 'C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/Daten/Meteorologie/jamtalhütte_20200401'
-
-# for filename in os.listdir(directory):
-#     if filename.endswith(".csv"):                       # Alternativ nach Excel daten suchen: (".xlsx")
-#      filename = os.path.join(directory, filename)
-
-#     if os.path.isfile (filename):
-#         print("files in path are:", filename)
-        
-
-
-
-# # Get CSV files list from a folder
-# path = 'C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/Daten/Meteorologie/jamtalhütte_20200401'
-# csv_files = glob.glob(path + "/*.csv")
-# print("csv Files are:'", csv_files)
-
-# # Read each CSV file into DataFrame
-# # This creates a list of dataframes
-# df_list = (pd.read_csv(file) for file in csv_files)
-# print("csvdfListe is:", df_list)
-
-# # # Concatenate all DataFrames
-# # big_df   = pd.concat(df_list, ignore_index=True)
-
-import os
-
-directory = os.fsencode('C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/Daten/Meteorologie/jamtalhütte_20200401')
+directory = os.fsencode('C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/Daten/Meteorologie/Jamtalhütte_20210824_20221004')
     
 for file in os.listdir(directory):
      filename = os.fsdecode(file)
-     if filename.endswith(".csv") or filename.endswith(".xlsx"): 
+     if filename.endswith(".csv"):                                  # or filename.endswith(".xlsx"): 
          # print(os.path.join(directory, filename))
          print(filename)
          continue
      else:
          continue
+print(directory) 
+        ######## Wie mache ich von hier aus weiter, dass ich nacheinander die Datein abarbeite? ##################### 
 
-        ######### Wie mache ich von hier aus weiter, dass ich nacheinander die Datein abarbeite? ##################### 
-        
+# for file in glob.iglob(os.path.join('C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/Daten/Meteorologie/Jamtalhuette_20131010_20221128', '*.zrx')):
+#     os.rename(filename, filename[:-4] + '.csv')
 
+folder = 'C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/Daten/Meteorologie/Jamtalhütte_20210824_20221004'
+for filename in os.listdir(folder):
+    infilename = os.path.join(folder,filename)
+    if not os.path.isfile(infilename): continue
+    oldbase = os.path.splitext(filename)
+    newname = infilename.replace('.zrx', '.csv')
+    output = os.rename(infilename, newname)
+
+print("Output:",output)
+
+print(directory) 
+print('done')    
