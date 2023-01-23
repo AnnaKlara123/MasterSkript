4d5f955f8f6661c9f8573b22b018867b8b1da980
Anna Stang <anna.stang@student.uibk.ac.at>
2023-01-23 16:32:55 +0100

    skirpt plottet mit -777 value

--- AI.py
+++ AI.py
@@ -3,17 +3,11 @@ import pandas as pd
 
 # Read data from a CSV file
 df = pd.read_csv("Precipitation_KItest.csv", delimiter=' ')
-print(df)
-
-# Convert the 'date' column to a datetime object
-#data['Date'] = pd.to_datetime(data['Date'])
-
-# date = df['Date'].tolist()
-# value = df["Value"].tolist()
 
 date = df['Date']
 date_time = pd.to_datetime(date, format='%Y%m%d%H%M%S')
 value = df['Value']
+df.replace(-777, pd.NaT, inplace = True)
 
 # print('date is:', date)
 # print('datetime ist:', date_time)
@@ -27,13 +21,13 @@ print(df)
 #data.insert(2, 'datetime', '22')   # (Spalte, Header, Value)
 
 # Set the 'date' column as the index
-df.set_index('Date', inplace=True)
+df.set_index('datetime', inplace=True)
 
 # Plot the values in the 'value' column over time
 plt.plot(df['Value'])
 
 # Add labels and a title to the plot
-plt.xlabel('Date')
+plt.xlabel('datetime')
 plt.ylabel('Value')
 plt.title('Values over Time')
 
