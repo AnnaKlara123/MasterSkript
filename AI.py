7e153eb453c9eb80f5f04c3e48cc24602f178ff3
Anna Stang <anna.stang@student.uibk.ac.at>
2023-01-23 16:37:50 +0100

    crazy shit

--- AI.py
+++ AI.py
@@ -7,32 +7,44 @@ df = pd.read_csv("Precipitation_KItest.csv", delimiter=' ')
 date = df['Date']
 date_time = pd.to_datetime(date, format='%Y%m%d%H%M%S')
 value = df['Value']
-df.replace(-777, pd.NaT, inplace = True)
-
-# print('date is:', date)
-# print('datetime ist:', date_time)
-# print('value is:', value)
 
 df.insert(2, 'datetime', date_time)   # (Spalte, Header, Value)
 
 print(df)
 
 
-#data.insert(2, 'datetime', '22')   # (Spalte, Header, Value)
-
 # Set the 'date' column as the index
-df.set_index('datetime', inplace=True)
+df.set_index(date_time, inplace=True)
 
-# Plot the values in the 'value' column over time
-plt.plot(df['Value'])
+# Replace all -9999 values with NaN
+df.replace(-777, pd.NaT, inplace = True)
 
-# Add labels and a title to the plot
+# # Convert the date column to a datetime object
+# df['datetime'] = pd.to_datetime(df['datetime'])
+
+# # Filter the dataframe to only include the past year
+# df = df[df['datetime'] > (pd.datetime.now() - pd.DateOffset(years=1))]
+
+# Plot the values
+plt.plot(df['datetime'], df['Value'])
+
+# Add labels to the x and y axes
 plt.xlabel('datetime')
 plt.ylabel('Value')
-plt.title('Values over Time')
 
 # Show the plot
 plt.show()
+
+# Plot the values in the 'value' column over time
+# plt.plot(df['Value'])
+
+# # Add labels and a title to the plot
+# plt.xlabel('datetime')
+# plt.ylabel('Value')
+# plt.title('Values over Time')
+
+# # Show the plot
+# plt.show()
 
 
 # import pandas as pd
