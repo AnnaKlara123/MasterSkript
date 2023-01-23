cb5e53cb4374a896e06ffbd951564404ecbeadaa
Anna Stang <anna.stang@student.uibk.ac.at>
2023-01-23 10:32:25 +0100

    mit .tolist() Daten aus Panda Dataframe ziehen

--- plot.py
+++ plot.py
@@ -8,10 +8,18 @@ from matplotlib import pyplot as plt
 filename = "C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/GitHubMasterSkripts/dfCSVfile2013cut.csv"
 data = pd.read_csv(filename, sep=' ', skiprows=5)
 
+
+########## Listen aus Panda Dataframe auslesen ############## --> Gibts da ne klügere Lösung?
+
 year = data["YY"].tolist()
+month = data["MM"].tolist()
+day = data["DD"].tolist()
+hour = data["HH"].tolist()
 value = data["Stat1"].tolist()
 
-print("year=",year)
+print("value=",year[:100])
+
+
 
 ###### plot data Winter 2013/14 #######
 
