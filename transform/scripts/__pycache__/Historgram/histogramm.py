3a27e9462e12a52b0f6248e9df1160fd50cda6f7
Anna Stang <anna.stang@student.uibk.ac.at>
2023-02-06 14:07:54 +0100

    Historgram works fine & saves in Plot foulder

--- transform/scripts/__pycache__/Historgram/histogramm.py
+++ transform/scripts/__pycache__/Historgram/histogramm.py
@@ -1,33 +1,51 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import os
+import glob
 
 # Load data into a DataFrame
-df = pd.read_csv('C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/GitHubMasterSkripts/MasterSkript/transform/scripts/Historgram/Soiltemp.csv',sep= "\t", engine='python', skiprows=4)
+df = pd.read_csv('C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/GitHubMasterSkripts/MasterSkript/transform/scripts/Historgram/Soiltemp1.csv',sep= "\t", engine='python', skiprows=4)
+
+file_path = "C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/GitHubMasterSkripts/MasterSkript/transform/scripts/Historgram/Soiltemp1.csv"
+file_name = os.path.basename(file_path).split('.')[0]
 
 # Get the column of interest
-
 header = df.columns
-# print(df)
-# print(header,"Header")
-
-df["Stat1"] = df["Stat1"].where(df["Stat1"] <= 45, other=np.nan)
 data = df.loc[:,"Stat1"]
 
-max_value = df["Stat1"].max()
-top_100 = df["Stat1"].nlargest(100)
-print("Max Value in Stat1=", max_value, "topten=",top_100)
+## Datenuntersuchung
+                    # max_value = df["Stat1"].max()
+                    # top_100 = df["Stat1"].nlargest(100)
+                    # df["Stat1"] = df["Stat1"].where(df["Stat1"] <= 45, other=np.nan)
+                    # print("Max Value in Stat1=", max_value, "topten=",top_100)
+                    # print(df)
+                    # print(header,"Header")
 
 # Plot histogram with 15 bins
-plt.hist(data, bins=100, color='red', edgecolor='black')
+plt.hist(data, bins=200, color='red', edgecolor='black')
 
 # Add labels and title
 plt.xlabel('Data Values')
 plt.ylabel('Frequency')
-plt.title('Histogram')
+plt.title(file_name)
+
+# Create the directory if it does not exist
+plot_dir = "plots"
+if not os.path.exists(plot_dir):
+    os.makedirs(plot_dir)
+
+# Save the plot to the directory
+plot_path = os.path.join(plot_dir, file_name + ".png")
+print("Plot will be saved to:", plot_path)
+plt.savefig(plot_path)
 
 # Show plot
 plt.show()
 
+print('done')
 
-#df.to_csv("test.csv", sep=' ', index=False)
+
+
+
+
