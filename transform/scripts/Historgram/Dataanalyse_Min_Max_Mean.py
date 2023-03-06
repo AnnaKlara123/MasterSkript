07c8ec8997c56ee0f6b01a4e682bda34cb73b2f8
Anna Stang <anna.stang@student.uibk.ac.at>
2023-03-06 15:01:33 +0100

    Parser works fine. Need to be saved and handl NaN Values!

--- transform/scripts/Historgram/Dataanalyse_Min_Max_Mean.py
+++ transform/scripts/Historgram/Dataanalyse_Min_Max_Mean.py
@@ -14,8 +14,8 @@ from termcolor import colored
 parser = argparse.ArgumentParser()
 parser.add_argument('--dir', type=str, help='The directory where the file is located', default='C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/GitHubMasterSkripts/MasterSkript/transform/output')
 parser.add_argument('--filename', type=str, help='The filename to read',  default='AirtempTest2102.csv')
-parser.add_argument('--year', type=int, help='The year to plot')
-parser.add_argument('--month', type=int, help='The month to plot')
+parser.add_argument('--year', type=int, help='The year to plot', default=2014)
+parser.add_argument('--month', type=int, help='The month to plot', default= 2)
 args = parser.parse_args()
 
 # Get the base filename
@@ -102,16 +102,17 @@ print(daily_data, 'dayly')
                     # monthly_mean = monthly_data['Stat1'].mean()
                     # #print(monthly_mean, "Monthly Mean Value")
 
-
 # Create a new figure for each month
 for month, data in monthly_data:
+    # Only plot the month that matches the specified month argument
+    if month.month != args.month:
+        continue
     
     # Group the data by day within the month
     daily_data = data.groupby(data['date'].dt.to_period('D'))
     
     # Calculate the maximum, minimum, and mean values for the month
     monthly_max = data['Stat1'].max()
-    print("Monthly max:",monthly_max)
     monthly_min = data['Stat1'].min()
     monthly_mean = data['Stat1'].mean()
     
@@ -121,19 +122,17 @@ for month, data in monthly_data:
     # Convert the PeriodIndex to a DatetimeIndex
     daily_means.index = daily_means.index.to_timestamp()
     
-    # # Create a new figure with a larger size
-    fig, ax = plt.subplots(figsize=(20, 12))
+    # Create a new figure for the month
+    fig, ax = plt.subplots(figsize=(10, 5))
     
-    # # # Plot the daily data for the month
-    # ax.plot(daily_means['date'], daily_means.values, label='Daily')
-    ax.scatter(daily_means.index, daily_means.values, color='black', marker='.')
-
-    # Plot the monthly maximum values
+    # Plot the daily data for the month
+    ax.plot(daily_means.index, daily_means.values, label='Daily')
+    
+ #     Plot the monthly maximum values
     ax.axhline(monthly_max, linestyle='--', color='red', label='Max')
-    
-    # Plot the monthly minimum values
     ax.axhline(monthly_min, linestyle='--', color='blue', label='Min')
-    
+    # Add a horizontal line for the mean value
+    ax.axhline(monthly_mean, linestyle='-', color='green', label='Mean')    
     # Add a horizontal line for the mean value
     ax.axhline(monthly_mean, linestyle='-', color='green', label='Mean')
     
@@ -144,13 +143,60 @@ for month, data in monthly_data:
     ax.set_ylabel('Value')
     
     # Set the title
-    ax.set_title(f'Monthly Data for {month}')
+    ax.set_title(f'Monthly Data for {month.strftime("%B %Y")}')
     
     # Add a legend
     ax.legend()
     
     # Show the plot
     plt.show()
+
+
+
+####################################Works perfekt for each month with dayly data plus max, min & mean ###################################################
+# # Create a new figure for each month
+# for month, data in monthly_data:
+    
+#     # Group the data by day within the month
+#     daily_data = data.groupby(data['date'].dt.to_period('D'))
+    
+#     # Calculate the maximum, minimum, and mean values for the month
+#     monthly_max = data['Stat1'].max()
+#     monthly_min = data['Stat1'].min()
+#     monthly_mean = data['Stat1'].mean()
+    
+#     # Calculate the daily averages for the month
+#     daily_means = daily_data['Stat1'].mean()
+
+#     # Convert the PeriodIndex to a DatetimeIndex
+#     daily_means.index = daily_means.index.to_timestamp()
+    
+#     # # Create a new figure with a larger size
+#     fig, ax = plt.subplots(figsize=(20, 12))
+    
+#     # # # Plot the daily data for the month
+#     ax.scatter(daily_means.index, daily_means.values, color='black', marker='.')
+
+#     # Plot the monthly maximum values
+#     ax.axhline(monthly_max, linestyle='--', color='red', label='Max')
+#     ax.axhline(monthly_min, linestyle='--', color='blue', label='Min')
+#     # Add a horizontal line for the mean value
+#     ax.axhline(monthly_mean, linestyle='-', color='green', label='Mean')
+    
+#     # Set the x-axis label
+#     ax.set_xlabel('Date')
+    
+#     # Set the y-axis label
+#     ax.set_ylabel('Value')
+    
+#     # Set the title
+#     ax.set_title(f'Monthly Data for {month}')
+    
+#     # Add a legend
+#     ax.legend()
+    
+#     # Show the plot
+#     plt.show()
 
 
 ###########################################################################################################
@@ -189,62 +235,3 @@ for month, data in monthly_data:
 # plt.show()
 
 ############################################################################################
-# def high_low_values(df):
-#     # Find the indices of the highest and lowest values in the dataframe
-#     highest_indices = sorted(range(len(df)), key=lambda i: df.iloc[i]['Stat1'])[-5:]
-#     lowest_indices = sorted(range(len(df)), key=lambda i: df.iloc[i]['Stat1'])[:5]
-
-#     # Get the corresponding dates for the highest and lowest values
-#     highest_dates = [df.iloc[i]['date'] for i in highest_indices]
-#     lowest_dates = [df.iloc[i]['date'] for i in lowest_indices]
-
-#     # Get the highest and lowest values
-#     highest_values = sorted(df['Stat1'])[-5:]
-#     lowest_values = sorted(df['Stat1'])[:5]
-
-#     # Create a string with the highest and lowest values and their corresponding dates
-#     highest_values_str = ', '.join([f'{value:.2f} ({date})' for value, date in zip(sorted(df['Stat1'])[-5:], highest_dates)])
-#     lowest_values_str = ', '.join([f'{value:.2f} ({date})' for value, date in zip(sorted(df['Stat1'])[:5], lowest_dates)])
-#     # Return the strings for the highest and lowest values
-#     return highest_values_str, lowest_values_str, highest_values, lowest_values
-
-
-# def histogram_plotter(df, plot_dir, file_name, year):
-#     data = df['Stat1'].replace(-9999, np.nan).values
-#     finite_data = data[np.isfinite(data)]
-#     hist, bins = np.histogram(finite_data, bins=100)
-#     bin_centers = (bins[1:] + bins[:-1]) / 2
-#     bar_width = bins[1] - bins[0]
-#     plt.bar(bin_centers, hist, width=bar_width, align='center')
-#     num_nan_values = df['Stat1'].isna().sum()
-#     if num_nan_values > 0:
-#         nan_bin_center = bin_centers.max() + bar_width
-#         plt.bar(nan_bin_center, num_nan_values, width=bar_width, align='center', color='gray')
-#         plt.text(nan_bin_center, num_nan_values, f'{num_nan_values}', ha='center', va='bottom')
-#     plt.title(f'Histogram for {year}')
-#     plt.xlabel('Stat1')
-#     plt.ylabel('Count')
-
-#     ### This could be smarter somewhere else! #####
-#     highest_values_str, lowest_values_str, highest_values, lowest_values = high_low_values(df)
-#     print(colored(f'Highest values of Stat1 are {highest_values_str}', 'red'))
-#     print(colored(f'Lowest values of Stat1 are {lowest_values_str}', 'blue'))
-    
-#     plt.text(0.02, 0.85, f'Highest values: {highest_values}', transform=plt.gca().transAxes, fontsize=10, verticalalignment='top')
-#     plt.text(0.02, 0.75, f'Lowest values: {lowest_values}', transform=plt.gca().transAxes, fontsize=10, verticalalignment='top')
-
-#     file_folder = os.path.join(plot_dir, f'Histogramm_{file_name[:-4]}')
-#     if not os.path.exists(file_folder):
-#         os.makedirs(file_folder)
-
-#     plot_name = f'Histogram_{file_name}{year}.png'
-#     plot_path = os.path.join(file_folder, plot_name)
-
-#     with tqdm(desc=f'Saving {plot_name}', total=1) as pbar:
-#         plt.savefig(plot_path)
-#         pbar.update()
-    
-#     plt.show()
-
-# # Call the histogram_plotter function
-# histogram_plotter(df, plot_dir, file_name, args.year)
