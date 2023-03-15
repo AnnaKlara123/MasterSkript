e5a90a7ca01159cbba4ff3c7a1dec7116e868d6c
Anna Stang <anna.stang@student.uibk.ac.at>
2023-03-15 12:43:56 +0100

    Skript with all Functions works fine!!

--- transform/scripts/Abfluss/Analys_Discharge.py
+++ transform/scripts/Abfluss/Analys_Discharge.py
@@ -53,8 +53,6 @@ def prepare_output(file_name, plot_dir):
 # Call the function and assign the result to a variable
 file_folder = prepare_output(file_name, args.dir)
 
-# print(file_folder)
-
 # Read in the CSV file
 df = pd.read_csv(file_path, sep=';')
 
@@ -211,4 +209,46 @@ yearly_totals_dict = total_discharge_all_years(df, unit_col, os.path.join(file_f
 for year, total in yearly_totals_dict.items():
     print(f'Total discharge for {year}: {total} {unit_lable}')
 
-#######################################################################################################
+##################  NaN Distribution #####################################################################################
+
+def NaN_distribution(df):
+    # Create a boolean mask indicating whether each value is NaN
+    mask = df.isna()
+
+    # Reduce the boolean mask to a boolean Series indicating whether each row contains NaN values
+    nan_mask = mask.any(axis=1)
+
+    # Index the DataFrame to get only the rows that contain NaN values
+    df_nan = df[nan_mask]
+
+    # Extract the date column from the DataFrame
+    dates = df_nan.index
+
+    # Create a line plot where the x-axis represents the dates and the y-axis represents the occurrence of NaN values
+    fig, ax = plt.subplots(figsize=(20, 5))
+    plt.plot(dates, [1]*len(dates), '.')
+    plt.title('NaN Values over Time')
+    plt.xlabel('Date')
+    plt.ylabel('NaN Value Occurrence')
+
+    # Loop over each date and annotate the corresponding dot with the date
+    # Add a label to the plot for each NaN value
+    annotated_dates = set()
+    for i, date in enumerate(dates):
+        if date.strftime('%d-%h-%Y') not in annotated_dates:
+            y_pos = 1 - (0.03 * len(annotated_dates)) # Adjust the y position based on the index of the annotation
+            ax.text(date, y_pos, f"{date.strftime('%d-%h-%Y')}", ha='center', fontsize=6)
+            annotated_dates.add(date.strftime('%d-%h-%Y'))
+
+    # Set the x-axis ticks and tick labels
+    ax.xaxis.set_major_locator(mdates.MonthLocator(interval=1))
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%m-%y'))
+    ax.xaxis.set_tick_params(labelsize=5)
+
+    # Set the y-axis tick labels
+    ax.set_yticks([0, 1])
+    ax.set_yticklabels(['No NaN', 'NaN'])
+
+    plt.show()
+
+NaN_distribution(df)
