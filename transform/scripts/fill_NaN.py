da2a6c1e55f7dbc6130704853b90d56e6c8ade19
Weltenbummlerin123 <anna.stang@student.uibk.ac.at>
2023-04-08 12:58:05 +0200

    parser of Lapsrate implemented

--- transform/scripts/fill_NaN.py
+++ transform/scripts/fill_NaN.py
@@ -1,71 +1,3 @@
-# import pandas as pd
-# import argparse 
-# import os
-
-# # Create the parser
-# parser = argparse.ArgumentParser()
-# parser.add_argument('--dir', type=str, help='The directory where the file is located', default="C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/GitHubMasterSkripts/MasterSkript/transform/output")
-# parser.add_argument('--df1', type=str, help='The Dataframe 1 ', default="Airtemp_nan.csv")
-# parser.add_argument('--hight1', type=str, help='The Dataframe 1 hight ', default="2141")
-# parser.add_argument('--df2', type=str, help='The Dataframe 2 ', default= 'lwd_Tirol_1197091-LT-BasisganglinieNaN.csv')
-# parser.add_argument('--hight2', type=str, help='The Dataframe 2 hight ', default="2997")
-# parser.add_argument('--freq', type=str, help='The frequency for the datetime index of the merged dataframe', default="5T")  # not working yet!
-# args = parser.parse_args()
-
-# # Get the base filename
-# file_namehyd = os.path.basename(args.df1)
-# file_nameldw = os.path.basename(args.df2)
-
-# # Build the full filepath from the directory and filename arguments
-# file_pathhyd = os.path.join(args.dir, args.df1)
-# file_pathlwd = os.path.join(args.dir, args.df2)
-
-# # Read in the CSV file
-# dfhyd = pd.read_csv(file_pathhyd, sep='\t')
-# dflwd = pd.read_csv(file_pathlwd, sep='\t')
-
-# # Combine the datetime columns into a single datetime object
-# dateshyd = [pd.Timestamp(int(row['YY']), int(row['MM']), int(row['DD']), int(row['HH']), int(row['MN'])) for i, row in dfhyd.iterrows()]
-# dateslwd = [pd.Timestamp(int(row['YY']), int(row['MM']), int(row['DD']), int(row['HH']), int(row['MN'])) for i, row in dflwd.iterrows()]
-
-# # Set the datetime index for the dataframe
-# dfhyd.index = dateshyd
-# dflwd.index = dateslwd
-
-# # # Create a new dataframe with a datetime index that spans the entire time range
-# # idx = pd.date_range(start=min(dfhyd.index.min(), dflwd.index.min()), end=max(dfhyd.index.max(), dflwd.index.max()), freq=args.freq)
-# # merged_df = pd.DataFrame(index=idx)
-# # Create a new dataframe with a datetime index that spans the entire time range
-# start_date = max(dfhyd.index.min(), dflwd.index.min())
-# end_date = max(dfhyd.index.max(), dflwd.index.max())
-# idx = pd.date_range(start=start_date, end=end_date, freq=args.freq)
-# merged_df = pd.DataFrame(index=idx)
-
-
-# # # Fill NaN values in dfhyd with values from dflwd
-# # dfhyd_filled = dfhyd.combine_first(dflwd)
-# # # Fill NaN values in dflwd with values from dfhyd
-# # dflwd_filled = dflwd.combine_first(dfhyd)
-
-# # Calculate the altitude difference between the two stations
-# alt_diff = int(args.hight2) - int(args.hight1)
-
-# # Calculate the lapse rate adjustment factor
-# lapse_rate = 6.5  # degrees per 1000m
-# adj_factor = 1 - (lapse_rate * alt_diff / 1000)
-
-# # Fill NaN values in dfhyd with values from dflwd, adjusted for lapse rate
-# dfhyd_filled = dfhyd.combine_first(dflwd * adj_factor)
-
-# # Fill NaN values in dflwd with values from dfhyd, adjusted for lapse rate
-# dflwd_filled = dflwd.combine_first(dfhyd * adj_factor)
-
-# # Save the filled dfhyd to a new CSV file
-# dfhyd_filled.to_csv(os.path.join(args.dir, f"filled_LR{args.freq}{file_namehyd}"), sep='\t', index=False)
-# dflwd_filled.to_csv(os.path.join(args.dir, f"filled_LR{args.freq}{file_nameldw}"), sep='\t', index=False)
-
-
-##################################################################################################################
 import pandas as pd
 import argparse 
 import os
@@ -78,6 +10,7 @@ parser.add_argument('--hight1', type=str, help='The Dataframe 1 hight ', default
 parser.add_argument('--df2', type=str, help='The Dataframe 2 ', default= 'lwd_Tirol_1197091-LT-BasisganglinieNaN_Test.csv')
 parser.add_argument('--hight2', type=str, help='The Dataframe 2 hight ', default="2997")
 parser.add_argument('--freq', type=str, help='The frequency for the datetime index of the merged dataframe', default="5T")  # not working yet!
+parser.add_argument('--lapsrate', type=str, help='The lapsrate that should be used', default="6.5")  # not working yet!
 args = parser.parse_args()
 
 # Get the base filename
@@ -112,9 +45,9 @@ merged_df = pd.DataFrame(index=idx)
 alt_diff = int(args.hight2) - int(args.hight1)
 
 # Calculate the lapse rate adjustment factor
-lapse_rate = 6.5  # degrees per 1000m
+lapse_rate = args.lapsrate  # degrees per 1000m
 adj_factor = 1 - (lapse_rate * alt_diff / 1000)
-print (adj_factor, "aF")
+print ("Adjustmentfactor: 1 - (lapse_rate * altitude_difference / 1000) =",adj_factor)
 
 # Fill NaN values in dfhyd with values from dflwd, adjusted for lapse rate (only for "Stat1" column)
 dfhyd_stat1 = dfhyd["Stat1"].combine_first(dflwd["Stat1"] - adj_factor)
