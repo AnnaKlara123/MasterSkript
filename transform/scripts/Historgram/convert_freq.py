78588cafb11597033acad25fe0831a4bcd7ed800
Weltenbummlerin123 <anna.stang@student.uibk.ac.at>
2023-04-17 16:26:02 +0200

    skript runs and converts all files in foulder to 10min frequency with mean values

--- transform/scripts/Historgram/convert_freq.py
+++ transform/scripts/Historgram/convert_freq.py
@@ -6,8 +6,11 @@ import pandas as pd
 parser = argparse.ArgumentParser()
 parser.add_argument('--dirin', type=str, help='The directory where the files are located', default="C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/GitHubMasterSkripts/MasterSkript/transform/output/convert_frequancy")
 parser.add_argument('--dirout', type=str, help='The directory where the files should be saved', default="C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/GitHubMasterSkripts/MasterSkript/transform/output/convert_frequancy/10min_Frequancy")
+parser.add_argument('--startdate', type=str, help='The startdate then the df shoud start', default='2014-09-01')
 args = parser.parse_args()
 
+# Define the desired start date
+start_date = pd.to_datetime(args.startdate)
 
 # Loop through all the files in the folder
 for file in os.listdir(args.dirin):
@@ -19,9 +22,8 @@ for file in os.listdir(args.dirin):
         # Combine the datetime columns into a single datetime object
         timestamp = [pd.Timestamp(int(row['YY']), int(row['MM']), int(row['DD']), int(row['HH']), int(row['MN'])) for i, row in df.iterrows()]
 
-        # Set the timestamp as the new DataFrame index
-        df = df.drop(['YY', 'MM', 'DD', 'HH', 'MN'], axis=1)
-        df.index = timestamp
+        # Convert the timestamp list to a DatetimeIndex object and set it as the new index
+        df.index = pd.DatetimeIndex(timestamp)
 
         # Convert the DataFrame values to float
         df = df.apply(pd.to_numeric, errors='coerce')
@@ -29,9 +31,29 @@ for file in os.listdir(args.dirin):
         # Resample the DataFrame to a 10-minute frequency using mean
         df = df.resample('10T').mean()
 
-        # Reset the index to columns and save the resampled DataFrame as a new CSV file
-        df = df.reset_index()
-        
-        #df = df.drop('timestamp')
+        # Update the MN column with the minutes from the new index
+        df['MN'] = df.index.minute
+        # Convert the YY, MM, DD, and HH columns to integers and remove the .0
+        df['YY'] = df.index.year.astype(int).astype(str)
+        df['MM'] = df.index.month.astype(int).astype(str)
+        df['DD'] = df.index.day.astype(int).astype(str)
+        df['HH'] = df.index.hour.astype(int).astype(str)
+        df['MN'] = df.index.minute.astype(int).astype(str)
+
+       # Define the new index with the desired start date and frequency
+        new_index = pd.date_range(start=start_date, periods=len(df), freq='10T')
+
+        # Reindex the DataFrame using the new index
+        df = df.reindex(new_index)
+
+        # Reset the index and drop the timestamp column. Save the resampled DataFrame as a new CSV file
+        df = df.reset_index().drop('index', axis=1)
+
+        ## # #####  create a header row CHANGE LAT, LONG & height FOR DIFFERENT STATIONS!#####################################
+        # # header_row = pd.DataFrame([['YY', 'MM', 'DD', 'HH', 'MN', 'STATIONNAEME'], ['YY', 'MM', 'DD', 'HH', 'MN', 'height'], ['YY', 'MM', 'DD', 'HH', 'MN', 'LONGNITUDE'], ['YY', 'MM', 'DD', 'HH', 'MN', 'LATITUDE'], ['YY', 'MM', 'DD', 'HH', 'MN', 'Stat1']], columns=['YY', 'MM', 'DD', 'HH', 'MN', 'Stat1'])
+        # # # concatenate the header row with the data frame
+        # # df = pd.concat([header_row, df], ignore_index=True)
+
         new_file_name = os.path.splitext(file)[0] + "_10min.csv"
         df.to_csv(os.path.join(args.dirout, new_file_name), sep='\t', index=False)
+        print("Saving:",new_file_name)
