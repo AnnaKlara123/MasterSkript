b24edc2684545d3c469c90ce105ef064d6f6e797
Weltenbummlerin123 <anna.stang@student.uibk.ac.at>
2023-04-20 12:55:41 +0200

    implement that all files in this script are beeing plottet, if a certain filename is not parsed

--- transform/scripts/Historgram/NaNPlots.py
+++ transform/scripts/Historgram/NaNPlots.py
@@ -16,15 +16,7 @@ parser.add_argument('--year', type=int, help='The year to plot')
 parser.add_argument('--month', type=int, help='The month to plot')
 args = parser.parse_args()
 
-# Get the base filename
-file_name = os.path.basename(args.filename)
-
-
-# Build the full filepath from the directory and filename arguments
-file_path = os.path.join(args.dir, args.filename)
-
-# Read in the CSV file
-df = pd.read_csv(file_path, sep='\t')
+count = 0
 
 # Create a subdirectory called 'plots' within the directory specified by --dir
 plot_dir = os.path.join(args.dir, 'plots')
@@ -32,125 +24,137 @@ if not os.path.exists(plot_dir):
     os.makedirs(plot_dir)
     print(f'Created directory: {colored(plot_dir, "green")}')
 
-# Create a folder for the current file if it doesn't exist
-file_folder = os.path.join(plot_dir, f'NaN_Plots{file_name[:-4]}')
-if not os.path.exists(file_folder):
-    os.makedirs(file_folder)
-    print(f'Created directory: {colored(file_folder, "green")}')
+for file_name in os.listdir(args.dir):
+    if file_name.endswith(".csv"):
+        count += 1
+        # Build the full filepath from the directory and filename arguments
+        file_path = os.path.join(args.dir, file_name)
 
+        # Read in the CSV file
+        df = pd.read_csv(file_path, sep='\t')
+        df.info()
 
-# Extract the values from the Stat1 column
-x = df['Stat1'].values
+        # Create a folder for the current file if it doesn't exist
+        file_folder = os.path.join(plot_dir, f'NaN_Plots{file_name[:-4]}')
+        if not os.path.exists(file_folder):
+            os.makedirs(file_folder)
+            print(f'Created directory: {colored(file_folder, "green")}')
 
-# Combine the datetime columns into a single datetime object
-dates = [pd.Timestamp(int(row['YY']), int(row['MM']), int(row['DD']), int(row['HH']), int(row['MN'])) for i, row in df.iterrows()]
+        # Extract the values from the Stat1 column
+        x = df['Stat1'].values
 
-# Set the datetime index for the dataframe
-df.index = dates
+        # Combine the datetime columns into a single datetime object
+        dates = [pd.Timestamp(int(row['YY']), int(row['MM']), int(row['DD']), int(row['HH']), int(row['MN'])) for i, row in df.iterrows()]
 
-# Remove the YY, MM, DD, HH, and MN columns from the dataframe
-df = df.drop(columns=['YY', 'MM', 'DD', 'HH', 'MN'])
+        # Set the datetime index for the dataframe
+        df.index = dates
 
-# Print the number of NaN values per year
-nan_count = df['Stat1'].isna().groupby(df.index.year).sum()
-print(colored('Number of NaN values per year:', 'green'), colored(nan_count, 'red'))
+        # Remove the YY, MM, DD, HH, and MN columns from the dataframe
+        df = df.drop(columns=['YY', 'MM', 'DD', 'HH', 'MN'])
 
-# Create a boolean mask of NaN values
-mask = df['Stat1'].isna()
+        # Print the number of NaN values per year
+        nan_count = df['Stat1'].isna().groupby(df.index.year).sum()
+        print(colored('Number of NaN values per year:', 'green'), colored(nan_count, 'red'))
 
-# Convert the boolean mask to 0 and 1 values
-plot_data = mask.astype(int)
+        # Create a boolean mask of NaN values
+        mask = df['Stat1'].isna()
 
-# Group the data by year and month
-groups = df.groupby([df.index.year, df.index.month])
+        # Convert the boolean mask to 0 and 1 values
+        plot_data = mask.astype(int)
 
-# Get the year and month from the command line arguments
-year = args.year
-month = args.month
+        # Group the data by year and month
+        groups = df.groupby([df.index.year, df.index.month])
 
-# If year and month are specified, plot data for the month of the year
-if year is not None and month is not None:
-    print(colored(f'plots data for {args.year}:{args.month}', 'blue'))
-    # Filter the dataframe by the year and month specified in the command line arguments
-    df_month = df[(df.index.year == year) & (df.index.month == month)]
+        # Get the year and month from the command line arguments
+        year = args.year
+        month = args.month
 
-    # Create a bar chart of the NaN values in the 'Stat1' column
-    fig, ax = plt.subplots(figsize=(20, 8))
-    plt.bar(df_month.index, plot_data.loc[df_month.index].values, width=0.001, color='red')
+        # If year and month are specified, plot data for the month of the year
+        if year is not None and month is not None:
+            print(colored(f'plots data for {args.year}:{args.month}', 'blue'))
+            # Filter the dataframe by the year and month specified in the command line arguments
+            df_month = df[(df.index.year == year) & (df.index.month == month)]
 
-    # Set the title and axis labels
-    plt.title(f'Occurrences of NaN values in {file_name} ({year}/{month:02d})')
-    plt.xlabel('Date')
-    plt.ylabel('NaN values')
+            # Create a bar chart of the NaN values in the 'Stat1' column
+            fig, ax = plt.subplots(figsize=(20, 8))
+            plt.bar(df_month.index, plot_data.loc[df_month.index].values, width=0.001, color='red')
 
-    # Set the x-axis ticks and tick labels
-    ax.xaxis.set_major_locator(mdates.DayLocator(interval=1))
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%d'))
+            # Set the title and axis labels
+            plt.title(f'Occurrences of NaN values in {file_name} ({year}/{month:02d})')
+            plt.xlabel('Date')
+            plt.ylabel('NaN values')
 
-    # Save the plot as a file
-    plot_filename = f'NaN_Plots_{file_name[:-4]}_{year}_{month:02d}.png'
-    plot_filepath = os.path.join(file_folder, plot_filename)
-    with tqdm(desc=f'Saving {plot_filename}', total=1) as pbar:
-        fig.savefig(plot_filepath)
-        pbar.update()
+            # Set the x-axis ticks and tick labels
+            ax.xaxis.set_major_locator(mdates.DayLocator(interval=1))
+            ax.xaxis.set_major_formatter(mdates.DateFormatter('%d'))
 
-    # Display the chart
-    #plt.show()
+            # Save the plot as a file
+            plot_filename = f'NaN_Plots_{file_name[:-4]}_{year}_{month:02d}.png'
+            plot_filepath = os.path.join(file_folder, plot_filename)
+            with tqdm(desc=f'Saving {plot_filename}', total=1) as pbar:
+                fig.savefig(plot_filepath)
+                pbar.update()
 
-# If year is specified, plot monthly data for the year
-elif year is not None:
-    print(colored(f'The year{args.year} is plotted', 'blue'))
-    # Filter the dataframe by the year specified in the command line argument
-    df_year = df[df.index.year == year]
+            # Display the chart
+            #plt.show()
 
-    # Create a bar chart of the NaN values in the 'Stat1' column
-    fig, ax = plt.subplots(figsize=(20, 8))
-    plt.bar(df_year.index, plot_data.loc[df_year.index].values, width=0.001, color='red')
+        # If year is specified, plot monthly data for the year
+        elif year is not None:
+            print(colored(f'The year{args.year} is plotted', 'blue'))
+            # Filter the dataframe by the year specified in the command line argument
+            df_year = df[df.index.year == year]
 
-    # Set the title and axis labels
-    plt.title(f'Occurrences of NaN values in{file_name} ({year})')
-    plt.xlabel('Month')
-    plt.ylabel('NaN values')
+            # Create a bar chart of the NaN values in the 'Stat1' column
+            fig, ax = plt.subplots(figsize=(20, 8))
+            plt.bar(df_year.index, plot_data.loc[df_year.index].values, width=0.001, color='red')
 
-    ax.xaxis.set_major_locator(mdates.MonthLocator(interval=1))
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
-    ax.tick_params(axis='x', labelsize=7)
-    
-    # Save the plot as a file
-    plot_filename = f'NaN_Plots_{file_name[:-4]}_{year}.png'
-    plot_filepath = os.path.join(file_folder, plot_filename)
-    with tqdm(desc=f'Saving {plot_filename}', total=1) as pbar:
-        fig.savefig(plot_filepath)
-        pbar.update()
+            # Set the title and axis labels
+            plt.title(f'Occurrences of NaN values in{file_name} ({year})')
+            plt.xlabel('Month')
+            plt.ylabel('NaN values')
 
-    # Display the chart
-    #plt.show()
+            ax.xaxis.set_major_locator(mdates.MonthLocator(interval=1))
+            ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+            ax.tick_params(axis='x', labelsize=7)
+            
+            # Save the plot as a file
+            plot_filename = f'NaN_Plots_{file_name[:-4]}_{year}.png'
+            plot_filepath = os.path.join(file_folder, plot_filename)
+            with tqdm(desc=f'Saving {plot_filename}', total=1) as pbar:
+                fig.savefig(plot_filepath)
+                pbar.update()
 
-# If neither year nor month is specified, plot data for the entire dataframe
-else:
-    print(colored('The whole Dataframe is plotted', 'blue'))
-    # Create a bar chart of the NaN values in the 'Stat1' column
-    fig, ax = plt.subplots(figsize=(20, 8))
-    plt.bar(df.index, plot_data.loc[df.index].values, width=0.01, color='red')
+            # Display the chart
+            #plt.show()
 
-    # Set the title and axis labels
-    plt.title(f'Occurrences of NaN values in {file_name}')
-    plt.xlabel('Date')
-    plt.ylabel('NaN values')
+        # If neither year nor month is specified, plot data for the entire dataframe
+        else:
+            print(colored('The whole Dataframe is plotted', 'blue'))
+            # Create a bar chart of the NaN values in the 'Stat1' column
+            fig, ax = plt.subplots(figsize=(20, 8))
+            plt.bar(df.index, plot_data.loc[df.index].values, width=0.01, color='red')
 
-    # Set the x-axis ticks and tick labels
-    ax.xaxis.set_major_locator(mdates.MonthLocator(interval=1))
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
-    plt.xticks(rotation=45)
-    ax.tick_params(axis='x', labelsize=7)
+            # Set the title and axis labels
+            plt.title(f'Occurrences of NaN values in {file_name}')
+            plt.xlabel('Date')
+            plt.ylabel('NaN values')
 
-    # Save the plot as a file
-    plot_filename = f'NaN_Plots_{file_name[:-4]}.png'
-    plot_filepath = os.path.join(file_folder, plot_filename)
+            # Set the x-axis ticks and tick labels
+            ax.xaxis.set_major_locator(mdates.MonthLocator(interval=1))
+            ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+            plt.xticks(rotation=45)
+            ax.tick_params(axis='x', labelsize=7)
 
-    with tqdm(desc=f'Saving {plot_filename}', total=1) as pbar:
-        fig.savefig(plot_filepath)
-        pbar.update()  # update the progress bar with a value between 0 and 1
+            # Save the plot as a file
+            plot_filename = f'NaN_Plots_{file_name[:-4]}.png'
+            plot_filepath = os.path.join(file_folder, plot_filename)
 
-    # Display the chart
-    #plt.show()
+            with tqdm(desc=f'Saving {plot_filename}', total=1) as pbar:
+                fig.savefig(plot_filepath)
+                pbar.update()  # update the progress bar with a value between 0 and 1
+
+            # Display the chart
+            #plt.show()
+
+# Print the number of files found and converted
+print("all", count, "files converted")
