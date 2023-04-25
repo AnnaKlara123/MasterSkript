e85d810f5c658b55a1d840948a384eefb8e46a19
Weltenbummlerin123 <anna.stang@student.uibk.ac.at>
2023-04-25 10:44:57 +0200

    Print NaN count. Count changes with the sum/mean calculation

--- transform/scripts/convert_freq.py
+++ transform/scripts/convert_freq.py
@@ -1,13 +1,14 @@
 import os
 import argparse
 import pandas as pd
+import numpy as np
 
 # Set the folder path where the CSV files are located
 parser = argparse.ArgumentParser()
-parser.add_argument('--dirin', type=str, help='The directory where the files are located', default="C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/GitHubMasterSkripts/MasterSkript/transform/output/convert_frequancy")
+parser.add_argument('--dirin', type=str, help='The directory where the files are located', default="C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/GitHubMasterSkripts/MasterSkript/transform/output/convert_frequancy/20.4Testdatasets")
 parser.add_argument('--dirout', type=str, help='The directory where the files should be saved', default= "C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/GitHubMasterSkripts/MasterSkript/transform/output/convert_frequancy/10min_Frequancy")
-parser.add_argument('--startdate', type=str, help='The startdate that the df shoud start', default='2014-09-01')
-parser.add_argument('--enddate', type=str, help='The enddate of the dataset', default='2021-09-01')
+parser.add_argument('--startdate', type=str, help='The startdate that the df shoud start',default='2013-10-11') #default='2014-09-01')
+parser.add_argument('--enddate', type=str, help='The enddate of the dataset', default='2013-10-14') #default='2021-09-01')
 args = parser.parse_args()
 
 # Define the desired start / end date
@@ -27,6 +28,8 @@ for file in os.listdir(args.dirin):
         # Read the CSV file into a pandas DataFrame
         df = pd.read_csv(os.path.join(args.dirin, file), delimiter='\t')
         df.info()
+        
+
 
         # Combine the datetime columns into a single datetime object
         timestamp = [pd.Timestamp(int(row['YY']), int(row['MM']), int(row['DD']), int(row['HH']), int(row['MN'])) for i, row in df.iterrows()]
@@ -35,15 +38,19 @@ for file in os.listdir(args.dirin):
         df.index = pd.DatetimeIndex(timestamp)
 
         # Convert the DataFrame values to float
-        df['Stat1'] = pd.to_numeric(df['Stat1'], errors='coerce')
+        df['Stat1'] = pd.to_numeric(df['Stat1'], errors='coerce') # convert any non-numeric values in the 'Stat1' column to NaN values
         df.info()
+         # Print the number of NaN values per year
+        nan_count = df['Stat1'].isna().groupby(df.index.year).sum()
+        print('Number of NaN values per year are:',nan_count)
 
      # Resample the Stat1 column to a 10-minute frequency using mean for non-Precipitation files, and sum for Precipitation files
         if "Precipitation" in file:
             df['Stat1'] = df['Stat1'].resample('10T').sum()
         else:
             df['Stat1'] = df['Stat1'].resample('10T').mean()
-       
+     # ACHTUNG! Wenn ein NaN Value im Original vorhanden ist, so wird der neue Wert beim resampling auch weiterhin NaN sein! 
+ 
        # Define new index and the desired start and end date and frequency
         new_index = pd.date_range(start=start_date, periods=len(df), freq='10T')
 
@@ -56,10 +63,14 @@ for file in os.listdir(args.dirin):
         df['HH'] = df.index.hour
         df['MN'] = df.index.minute
         df.info()
-
+        
         df = df.truncate(before=start_date, after=end_date)  # Truncate the data to the desired start and end dates
         # Reset the index and drop the timestamp column
         df.reset_index(drop=True, inplace=True)
+        # Print the number of NaN values per year
+        nan_count = df['Stat1'].isna().sum()
+        print('Number of NaN values per year are:',nan_count)
+
 
         ## # #####  create a header row CHANGE LAT, LONG & height FOR DIFFERENT STATIONS!#####################################
         # # header_row = pd.DataFrame([['YY', 'MM', 'DD', 'HH', 'MN', 'STATIONNAEME'], ['YY', 'MM', 'DD', 'HH', 'MN', 'height'], ['YY', 'MM', 'DD', 'HH', 'MN', 'LONGNITUDE'], ['YY', 'MM', 'DD', 'HH', 'MN', 'LATITUDE'], ['YY', 'MM', 'DD', 'HH', 'MN', 'Stat1']], columns=['YY', 'MM', 'DD', 'HH', 'MN', 'Stat1'])
