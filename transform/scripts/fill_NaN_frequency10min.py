745192437bf8b0a4b1bbc0bdb425bbaf9c122db1
Weltenbummlerin123 <anna.stang@student.uibk.ac.at>
2023-04-26 14:16:05 +0200

    filling of t-1 for Wind

--- transform/scripts/fill_NaN_frequency10min.py
+++ transform/scripts/fill_NaN_frequency10min.py
@@ -2,6 +2,8 @@ import pandas as pd
 import argparse 
 import os
 from termcolor import colored
+from datetime import timedelta
+from pandas import np
 
 # # Create the parser
 # parser = argparse.ArgumentParser()
@@ -14,9 +16,9 @@ from termcolor import colored
 # args = parser.parse_args()
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dir', type=str, help='The directory where the file is located', default="C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/GitHubMasterSkripts/MasterSkript/transform/output")
-parser.add_argument('--df1', type=str, help='The Dataframe 1. DF', default="hd_Precipitation_NaN_10minTest.csv")
-parser.add_argument('--df2', type=str, help='The Dataframe 2 DF', default= 'ZAMG_Precipitation_NaN_10minTest.csv')
+parser.add_argument('--dir', type=str, help='The directory where the file is located', default="C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/GitHubMasterSkripts/MasterSkript/transform/output/convert_frequancy/10min_Frequancy")
+parser.add_argument('--df1', type=str, help='The Dataframe 1. DF', default="hd_Windspeed_NaN_10minTest.csv")
+parser.add_argument('--df2', type=str, help='The Dataframe 2 DF', default= 'lwd_Tirol_GH_1197091-WG-BasisganglinieNaN_10minTest.csv')
 #parser.add_argument('--df3', type=str, help='The Dataframe 3 DF', default= 'ZAMG_Precipitation_NaN_10minTEST.csv')
 #parser.add_argument('--lapsrate', type=str, help='The lapsrate that should be used. Use 1, if you want a 1:1 filling.', default= "6.5")  
 args = parser.parse_args()
@@ -33,7 +35,9 @@ file_path2 = os.path.join(args.dir, args.df2)
 
 # Read in the CSV file
 df1 = pd.read_csv(file_path1, sep='\t')
+#df1.info()
 df2 = pd.read_csv(file_path2, sep='\t')
+#df2.info()
 #df3 = pd.read_csv(file_path3, sep='\t')
 
 
@@ -47,61 +51,39 @@ timestamp2 = [pd.Timestamp(int(row['YY']), int(row['MM']), int(row['DD']), int(r
 # Convert the timestamp list to a DatetimeIndex object and set it as the new index
 df2.index = pd.DatetimeIndex(timestamp2)
 
-# Combine the dataframes to fill in missing values in df1
-combined_df1 = df1.combine_first(df2)
-combined_df2 =df2.combine_first(df1)
+# # Combine the dataframes to fill in missing values in df1
+# combined_df1 = df1.combine_first(df2)
+# combined_df2 = df2.combine_first(df1)
+
+# Fill NaN values using the difference calculation in df1
+for col in df1.columns:
+    for i, val in df1[col].items():
+        if pd.isna(val):
+            # Check if the timestamps exist in both dataframes
+            if i - pd.Timedelta(minutes=10) in df1.index and i + pd.Timedelta(minutes=10) in df2.index:
+                diff = df1.loc[i - pd.Timedelta(minutes=10), col] - df2.loc[i - pd.Timedelta(minutes=10), col]
+                if diff > 0:
+                    df1.loc[i, col] = df2.loc[i, col] - diff
+                else:
+                    df1.loc[i, col] = df2.loc[i, col] + np.abs(diff)
+
+# Fill NaN values using the difference calculation in df2
+for col in df2.columns:
+    for i, val in df2[col].items():
+        if pd.isna(val):
+            # Check if the timestamps exist in both dataframes
+            if i - pd.Timedelta(minutes=10) in df2.index and i + pd.Timedelta(minutes=10) in df1.index:
+                diff = df2.loc[i + pd.Timedelta(minutes=10), col] - df1.loc[i - pd.Timedelta(minutes=10), col]
+                if diff > 0:
+                    df2.loc[i, col] = df1.loc[i, col] - diff
+                else:
+                    df2.loc[i, col] = df1.loc[i, col] + np.abs(diff)
+
 
 # save the filled dataframe as a CSV file
-combined_df1.to_csv(os.path.join(args.dir, f"filled_{file_name1}"), sep='\t', index=False)
+df1.to_csv(os.path.join(args.dir, f"filled_{file_name1}"), sep='\t', index=False)
 print(colored(" Dataframe 1 is filled and saved", "yellow"))
+
 # save the filled dataframe as a CSV file
-combined_df2.to_csv(os.path.join(args.dir, f"filled_{file_name2}"), sep='\t', index=False)
+df2.to_csv(os.path.join(args.dir, f"filled_{file_name2}"), sep='\t', index=False)
 print(colored("Dataframe 2 is filled and saved", "yellow"))
-
-
-# filled_low_freq = fill_low_freq(low_freq, high_freq, freq_ratio, adj_factor)
-# # save the filled dataframe as a CSV file
-# filled_low_freq.to_csv(os.path.join(args.dir, f"filled_{file_name1}"), sep='\t', index=False)
-# print(colored("Low-Frequency Dataframe is filled and saved", "yellow"))
-
-
-# # ### Fill in high Frequencay DF #### WORKS!! 
-# def fill_high_freq(low_freq, high_freq):
-#     import pandas as pd
-
-#     # create a new datetime index with 1-minute intervals
-#     new_index = pd.date_range(low_freq.index[0], low_freq.index[-1], freq='1T')
-
-#     # create a new DataFrame with the new index and fill it with NaN values
-#     new_low_freq = pd.DataFrame(index=new_index, columns=low_freq.columns).fillna(method='ffill')
-
-#     # update the values of the new DataFrame with values from the original low-frequency DataFrame
-#     for i, row in low_freq.iterrows():
-#         new_val = row['Stat1'] / 10
-#         start_time = i
-#         end_time = start_time + pd.Timedelta(minutes=10)
-#         new_low_freq.loc[start_time:end_time, 'Stat1'] = new_val
-
-#     # update the index of the new DataFrame to match the format of the original low-frequency DataFrame
-#     new_low_freq['YY'] = new_low_freq.index.year
-#     new_low_freq['MM'] = new_low_freq.index.month
-#     new_low_freq['DD'] = new_low_freq.index.day
-#     new_low_freq['HH'] = new_low_freq.index.hour
-#     new_low_freq['MN'] = new_low_freq.index.minute
-
-#     # fill NaN values in the high-frequency dataframe with values from the low-frequency dataframe
-#     high_freq['Stat1'].fillna((new_low_freq['Stat1']/10).interpolate(method='time'), inplace=True)
-#     # The interpolate() method uses linear interpolation to fill NaN values. By specifying the method='time' argument, it will interpolate based on the time index, so each minute will get the corresponding value of the other dataframe.
-    
-#     #####  create a header row CHANGE LAT, LONG & height FOR DIFFERENT STATIONS!#####################################
-#     header_row = pd.DataFrame([['YY', 'MM', 'DD', 'HH', 'MN', 'STATIONNAEME'], ['YY', 'MM', 'DD', 'HH', 'MN', args.height2], ['YY', 'MM', 'DD', 'HH', 'MN', 'LONGNITUDE'], ['YY', 'MM', 'DD', 'HH', 'MN', 'LATITUDE'], ['YY', 'MM', 'DD', 'HH', 'MN', 'Stat1']], columns=['YY', 'MM', 'DD', 'HH', 'MN', 'Stat1'])
-
-#     # concatenate the header row with the data frame
-#     high_freq = pd.concat([header_row, high_freq], ignore_index=True)
-#     return high_freq
-
-# filled_high_freq = fill_high_freq(low_freq, high_freq)
-
-# # save the filled dataframe as a CSV file
-# filled_high_freq.to_csv(os.path.join(args.dir, f"filled_{file_name2}"), sep='\t', index=False)
-# print(colored("High-Frequency Dataframe is filled and saved", "yellow"))
