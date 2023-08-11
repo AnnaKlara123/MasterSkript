46f556a784381257a29a6936cf14e62a9f3d85e8
Weltenbummlerin123 <anna.stang@student.uibk.ac.at>
2023-08-11 14:21:51 +0200

    End Date anpassen! Das war vorher falsch.. bzw. hast sich "Selbstständig gemacht"

--- transform/scripts/convert_freq.py
+++ transform/scripts/convert_freq.py
@@ -5,10 +5,10 @@ import numpy as np
 
 # Set the folder path where the CSV files are located
 parser = argparse.ArgumentParser()
-parser.add_argument('--dirin', type=str, help='The directory where the files are located', default="C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/GitHubMasterSkripts/MasterSkript/transform/output/convert_frequancy/Testdatasets")
-parser.add_argument('--dirout', type=str, help='The directory where the files should be saved', default= "C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/GitHubMasterSkripts/MasterSkript/transform/output/convert_frequancy/Testdatasets")
-parser.add_argument('--startdate', type=str, help='The startdate that the df shoud start',default='2014-09-01')
-parser.add_argument('--enddate', type=str, help='The enddate of the dataset', default='2022-09-30')
+parser.add_argument('--dirin', type=str, help='The directory where the files are located', default="C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/WaSiM_Execution0908/Input/hyd")
+parser.add_argument('--dirout', type=str, help='The directory where the files should be saved', default= "C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/WaSiM_Execution0908/Input/hyd")
+parser.add_argument('--startdate', type=str, help='The startdate that the df shoud start',default='2019-6-26')
+parser.add_argument('--enddate', type=str, help='The enddate of the dataset', default='2022-8-19')
 args = parser.parse_args()
 
 # Define the desired start / end date
@@ -45,15 +45,18 @@ for file in os.listdir(args.dirin):
         print('Number of NaN values per year are:',nan_count)
 
      # Resample the Stat1 column to a 10-minute frequency using mean for non-Precipitation files, and sum for Precipitation files
-        if "Precipitation" in file:
+        if "Precipitation" or "disch" in file:
              df['Stat1'] = df['Stat1'].resample('10T').apply(lambda x: np.nan if x.isnull().sum() > 5 else x.sum())
              # Wenn mehr als 5 Werte NaN sind wird das Ergebnis auch NaN sein
         else:
             df['Stat1'] = df['Stat1'].resample('10T').mean()
      # ACHTUNG! Wenn ein NaN Value im Original vorhanden ist, so wird der neue Wert beim resampling auch weiterhin NaN sein! 
+
+     # Round the 'Stat1' values to a certain number of decimal places (e.g., 2 decimal places)
+        df['Stat1'] = df['Stat1'].round(3)
  
        # Define new index and the desired start and end date and frequency
-        new_index = pd.date_range(start=start_date, periods=len(df), freq='10T')
+        new_index = pd.date_range(start=start_date,end=end_date, freq='10T')
 
         # Reindex the DataFrame using the new index
         df = df.reindex(new_index)
