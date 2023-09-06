50aca491bbc6edaa520c32c84949d7ff50f6b7b8
Weltenbummlerin123 <anna.stang@student.uibk.ac.at>
2023-09-06 11:42:14 +0200

    Multimerge mit neuer NAN Spalte =-9999 und auf 2 digits gerundet

--- transform/scripts/multicombine.py
+++ transform/scripts/multicombine.py
@@ -8,11 +8,11 @@ from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dir', type=str, help='The directory where the file is located',
-                    default="C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/GitHubMasterSkripts/MasterSkript/transform/output/WaSiM_Combined_files/WaSiM_Humidity/merged/")
+                    default="C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/GitHubMasterSkripts/MasterSkript/transform/output/WaSiM_Combined_files/WaSiM_Globalrad/")
 parser.add_argument('--df_low', type=str, help='The Dataframe Valley',
-                    default="merged_df_Humidity.csv")
+                    default="hd_GlobalradiationNEW_NaN_10min.csv")
 parser.add_argument('--df_high', type=str, help='The Dataframe Peak',
-                    default='filled_lwd_Tirol_GH_1197091-LF-BasisganglinieNaN_10min_2022.csv')
+                    default='merged_df_Globalrad.csv')
 # parser.add_argument('--df3', type=str, help='The Dataframe 3 DF', default= 'ZAMG_Precipitation_NaN_10minTEST.csv')
 # parser.add_argument('--lapsrate', type=str, help='The lapsrate that should be used. Use 1, if you want a 1:1 filling.', default= "6.5")
 args = parser.parse_args()
@@ -33,6 +33,7 @@ file_name_high = os.path.basename(args.df_high)
 file_path_low = os.path.join(args.dir, args.df_low)
 file_path_high = os.path.join(args.dir, args.df_high)
 
+
 # Read the CSV files
 df_low = pd.read_csv(file_path_low, sep='\t')
 df_high = pd.read_csv(file_path_high, sep='\t')
@@ -40,22 +41,25 @@ df_high = pd.read_csv(file_path_high, sep='\t')
 # Merge the dataframes based on common timestamp columns
 merged_df = pd.merge(df_low, df_high, on=['YY', 'MM', 'DD', 'HH', 'MN'])
 
-# # Rename columns in df_low to avoid conflicts
-# df_low.rename(columns={'Stat1': 'Stat1_low', 'Stat2': 'Stat2_low'}, inplace=True)
+# Add a fictional column 'Stat4' filled with NaN values
+merged_df['Stat4'] = -9999
 
-# # Rename columns in df_high to avoid conflicts
-# df_high.rename(columns={'Stat1': 'Stat1_high'}, inplace=True)
+# Round all numeric columns to 2 decimal places
+merged_df = merged_df.round(2)
+# Convert all NaN values to -9999
+merged_df = merged_df.fillna(-9999)
 
-# # Concatenate the dataframes horizontally
-# final_df = pd.concat([merged_df[['YY', 'MM', 'DD', 'HH', 'MN', 'Stat1_x', 'Stat2', 'Stat1_y']],
-#                       df_low[['YY', 'MM', 'DD', 'HH', 'MN', 'Stat1_low']],
-#                       df_high[['YY', 'MM', 'DD', 'HH', 'MN', 'Stat1_high']]], axis=1)
 
 # Save the merged DataFrame to a CSV file
-output_filename = "multimerge_HUmidity.csv"
+output_filename = "multimerge_Airtemp.csv"
 output_filepath = os.path.join(args.dir, output_filename)
 merged_df.to_csv(output_filepath, sep='\t', index=False)
 
 print(f"Merged DataFrame saved to {output_filepath}")
 
 print("done")
+
+
+
+
+
