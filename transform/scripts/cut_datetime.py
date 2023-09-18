25fc3f969d1a75fbeed0b7705a755e7a110ce108
Weltenbummlerin123 <anna.stang@student.uibk.ac.at>
2023-09-18 11:48:58 +0200

    Add Progress Bar

--- transform/scripts/cut_datetime.py
+++ transform/scripts/cut_datetime.py
@@ -2,19 +2,27 @@ import os
 import argparse
 import pandas as pd
 import numpy as np
+from tqdm import tqdm  # Import tqdm for the progress bar
 
 # Set the folder path where the CSV files are located
 parser = argparse.ArgumentParser()
-parser.add_argument('--dirin', type=str, help='The directory where the files are located', default="C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/GitHubMasterSkripts/MasterSkript/transform/output/WaSiM_Combined_files/WaSiM_Humidity")
-parser.add_argument('--dirout', type=str, help='The directory where the files are located', default="C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/GitHubMasterSkripts/MasterSkript/transform/output/WaSiM_Combined_files/WaSiM_Humidity")
-parser.add_argument('--startdate', type=str, help='The startdate that the df shoud start',default='2014-09-01')
-parser.add_argument('--enddate', type=str, help='The enddate of the dataset', default='2022-05-30')
+parser.add_argument('--dirin', type=str, help='The directory where the files are located', default="C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/Meteo_Discharge")
+parser.add_argument('--dirout', type=str, help='The directory where the files are located', default="C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/Meteo_Discharge/Cut")
+parser.add_argument('--startdate', type=str, help='The startdate that the df shoud start',default='2019-06-26')
+parser.add_argument('--enddate', type=str, help='The enddate of the dataset', default='2022-08-19')
 
 args = parser.parse_args()
 
 # Define the desired start / end date
 start_date = pd.to_datetime(args.startdate)
 end_date = pd.to_datetime(args.enddate)
+
+# Get a list of CSV files in the input directory
+csv_files = [file for file in os.listdir(args.dirin) if file.endswith(".csv")]
+
+# Initialize the progress bar
+progress_bar = tqdm(total=len(csv_files), desc='Processing CSV files')
+
 
 # Loop through all the files in the folder
 for file in os.listdir(args.dirin):
@@ -38,6 +46,12 @@ for file in os.listdir(args.dirin):
         # Save the modified DataFrame to a new CSV file
         new_file_name = os.path.splitext(file)[0] + "_cut.csv"
         df.to_csv(os.path.join(args.dirout, new_file_name), sep='\t', index=False)
+        
+        # Update the progress bar
+        progress_bar.update(1)
+
+# Close the progress bar
+progress_bar.close()
 
 # Print a message indicating that the script has finished running
 print("Script completed!")
