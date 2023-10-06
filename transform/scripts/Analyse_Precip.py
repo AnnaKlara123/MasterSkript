6b80cf4650f724c0257c505a2d6521bbde4b832a
Weltenbummlerin123 <anna.stang@student.uibk.ac.at>
2023-10-06 16:30:19 +0200

    Filter for rain periods

--- transform/scripts/Analyse_Precip.py
+++ transform/scripts/Analyse_Precip.py
@@ -21,6 +21,7 @@ count = 0
 # Create an empty DataFrame to store the daily sum values
 daily_sum_df = pd.DataFrame(columns=['Date', 'Sum'])
 
+
 # Loop through all the files in the folder
 for file in os.listdir(args.dirin):
     # Check if the file is a CSV file
@@ -61,7 +62,35 @@ top_20_days = daily_sum_df_sorted.head(20)
 print("Top 20 days with the highest sum values:")
 print(top_20_days)
 
-# Save the top 20 days to a new CSV file
-output_csv_path = os.path.join(args.dirout, "top_20_days.csv")
-top_20_days.to_csv(output_csv_path)
+# Save the top 20 days to a new CSV file without index and in a cleaner format
+output_csv_path = os.path.join(args.dirout, "top_20_days.txt")
+top_20_days.to_csv(output_csv_path, index=False, header=['Date', 'Sum'])
 print(f"Top 20 days saved to {output_csv_path}")
+
+
+
+
+
+## Filter periods of rainfall for at least 3 days in a row
+rain_periods = []
+current_rain_period = []
+current_rain_sum = 0  # Variable to keep track of the current rain period's total sum
+for _, row in daily_sum_df.iterrows():
+    if row['Sum'] > 0:
+        current_rain_period.append(row['Date'])
+        current_rain_sum += row['Sum']
+    else:
+        if len(current_rain_period) >= 3:
+            rain_periods.append({'Period': current_rain_period, 'Total Rainfall': current_rain_sum})
+        current_rain_period = []
+        current_rain_sum = 0
+
+# Save the rain periods to a file
+rain_periods_output_path = os.path.join(args.dirout, "rain_periods.txt")
+with open(rain_periods_output_path, 'w') as f:
+    for i, period in enumerate(rain_periods, start=1):
+        f.write(f"Rain Period {i}:\n")
+        f.write(f"Start Date: {period['Period'][0]}\n")
+        f.write(f"End Date: {period['Period'][-1]}\n")
+        f.write(f"Total Rainfall: {period['Total Rainfall']} mm\n\n")
+print(f"Rain periods saved to {rain_periods_output_path}")
