26af1a05a162c40278d7a6119e8d56f104b0689e
Weltenbummlerin123 <anna.stang@student.uibk.ac.at>
2023-10-20 14:51:01 +0200

    Add Plott for mean diycharge top100 days

--- transform/scripts/Analyse_Precip.py
+++ transform/scripts/Analyse_Precip.py
@@ -1,148 +1,181 @@
-import os
-import argparse
+# import os
+# import argparse
+# import pandas as pd
+# from tqdm import tqdm
+
+# # Set the folder path where the CSV files are located
+# parser = argparse.ArgumentParser()
+# parser.add_argument('--dirin', type=str, help='The directory where the files are located',
+#                     default="C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/Meteo_Discharge/AnalyseTest_0610/Discharge")
+# parser.add_argument('--dirout', type=str, help='The directory where the files should be saved',
+#                     default="C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/Meteo_Discharge/AnalyseTest_0610/Discharge")
+# args = parser.parse_args()
+
+# count = 0
+
+# # Create an empty DataFrame to store the daily sum values
+# daily_sum_df = pd.DataFrame(columns=['Date', 'Sum'])
+
+# # Create an empty DataFrame to store the hourly sum values
+# hourly_sum_df = pd.DataFrame(columns=['Date', 'Sum'])
+
+# # Loop through all the files in the folder
+# for file in os.listdir(args.dirin):
+#     # Check if the file is a CSV file
+#     if file.endswith(".csv"):
+#         # Increment the counter variable
+#         count += 1
+
+#         # Read the CSV file into a pandas DataFrame
+#         df = pd.read_csv(os.path.join(args.dirin, file), delimiter='\t')
+#         # Extract the base name of the input file without the extension
+#         input_file_name = os.path.splitext(os.path.basename(file))[0]
+
+
+#         # Combine the datetime columns into a single datetime object
+#         timestamp = [pd.Timestamp(int(row['YY']), int(row['MM']), int(row['DD']), int(row['HH']), int(row['MN'])) for i, row in df.iterrows()]
+
+#         # Convert the timestamp list to a DatetimeIndex object and set it as the new index
+#         df.index = pd.DatetimeIndex(timestamp)
+
+#         # Convert the DataFrame values to float
+#         df['Stat1'] = pd.to_numeric(df['Stat1'], errors='coerce')
+
+#         # Calculate the daily sum/mean for the period between April 20th and October 20th each year (summer months)
+#         df_filtered = df[(df.index.month >= 4) & (df.index.month <= 10) | ((df.index.month == 4) & (df.index.day >= 20)) | ((df.index.month == 10) & (df.index.day <= 20))]
+#         daily_sum = df_filtered['Stat1'].resample('D').mean()
+
+
+#         # Create a DataFrame for daily_sum with 'Date' and 'Sum' columns
+#         daily_sum = daily_sum.reset_index()
+#         daily_sum.columns = ['Date', 'Sum']
+
+#         # Append the daily_sum to the daily_sum_df
+#         daily_sum_df = pd.concat([daily_sum_df, daily_sum], axis=0)
+
+#         # Calculate the hourly sum for the period between April 20th and October 20th each year
+#         hourly_sum = df_filtered['Stat1'].resample('H').sum()
+
+#         # Create a DataFrame for hourly_sum with 'Date' and 'Sum' columns
+#         hourly_sum = hourly_sum.reset_index()
+#         hourly_sum.columns = ['Date', 'Sum']
+
+#         # Append the hourly_sum to the hourly_sum_df
+#         hourly_sum_df = pd.concat([hourly_sum_df, hourly_sum], axis=0)
+
+
+# # Sort the daily_sum_df by the 'Sum' column in descending order
+# daily_sum_df_sorted = daily_sum_df.sort_values(by='Sum', ascending=False)
+
+# # Get the top 20 days with the highest sum values
+# top_20_days = daily_sum_df_sorted.head(100)
+
+# top_20_days['Sum'] = top_20_days['Sum'].round(2)
+
+# # Print the top 20 days
+# print("Top 20 days with the highest sum values:")
+# print(top_20_days)
+
+# # Save the top 20 days to a new CSV file without index and in a cleaner format
+# output_csv_path = os.path.join(args.dirout, f"top_20_days_{input_file_name}.txt")
+# top_20_days.to_csv(output_csv_path, index=False, header=['Date', 'Sum'])
+# print(f"Top 20 days saved to {output_csv_path}")
+
+
+# # Sort the hourly_sum_df by the 'Sum' column in descending order
+# hourly_sum_df_sorted = hourly_sum_df.sort_values(by='Sum', ascending=False)
+
+# # Get the top 20 hours with the highest sum values
+# top_20_hours = hourly_sum_df_sorted.head(20)
+
+# # Round the 'Sum' values to 2 decimal places
+# top_20_hours['Sum'] = top_20_hours['Sum'].round(2)
+
+# # Save the top 20 hours to a new CSV file without index and header
+# output_top_20_hours_path = os.path.join(args.dirout, f"top_20_hours_{input_file_name}.txt")
+# if not top_20_hours.empty:
+#     top_20_hours.to_csv(output_top_20_hours_path, index=False, header=['Date', 'Sum'])
+#     print(f"Top 20 hours with the highest sum values saved to {output_top_20_hours_path}")
+# else:
+#     print("No data available for the specified period.")
+
+
+
+
+# ## Filter periods of rainfall for at least 3 days in a row
+# rain_periods = []
+# current_rain_period = []
+# current_rain_sum = 0  # Variable to keep track of the current rain period's total sum
+# for _, row in daily_sum_df.iterrows():
+#     if row['Sum'] > 0:
+#         current_rain_period.append(row['Date'])
+#         current_rain_sum += row['Sum']
+#     else:
+#         if len(current_rain_period) >= 3:
+#             current_rain_sum = round(current_rain_sum, 2)
+#             rain_periods.append({'Period': current_rain_period, 'Total Rainfall': current_rain_sum})
+#         current_rain_period = []
+#         current_rain_sum = 0
+
+# # Save the rain periods to a file
+# rain_periods_output_path = os.path.join(args.dirout, "rain_periods.txt")
+# with open(rain_periods_output_path, 'w') as f:
+#     for i, period in enumerate(rain_periods, start=1):
+#         f.write(f"Rain Period {i}:\n")
+#         f.write(f"Start Date: {period['Period'][0]}\n")
+#         f.write(f"End Date: {period['Period'][-1]}\n")
+#         f.write(f"Total Rainfall: {period['Total Rainfall']} mm\n\n")
+# print(f"Rain periods saved to {rain_periods_output_path}")
+
+
+# # Convert the 'Date' columns in both DataFrames to a common date format
+# top_20_days['Date'] = top_20_days['Date'].dt.strftime('%Y-%m-%d')
+# top_20_hours['Date'] = top_20_hours['Date'].dt.strftime('%Y-%m-%d')
+
+# # Check if any of the dates in top_20_days match any of the dates in top_20_hours
+# matching_dates = top_20_days['Date'].isin(top_20_hours['Date'])
+
+# # Get the matching days
+# matching_days = top_20_days[matching_dates]
+
+# # Save the matching days to a CSV file
+# output_matching_days_path = os.path.join(args.dirout, "matching_top_days.txt")
+
+# if not matching_days.empty:
+#     matching_days.to_csv(output_matching_days_path, index=False)
+#     print(f"Matching days saved to {output_matching_days_path}")
+# else:
+#     print("No matching days found between top 20 days and top 20 hours, so no file was saved.")
+
+
 import pandas as pd
-from tqdm import tqdm
+import matplotlib.pyplot as plt
 
-# Set the folder path where the CSV files are located
-parser = argparse.ArgumentParser()
-parser.add_argument('--dirin', type=str, help='The directory where the files are located',
-                    default="C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/Meteo_Discharge/AnalyseTest_0610/Precipitation")
-parser.add_argument('--dirout', type=str, help='The directory where the files should be saved',
-                    default="C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/Meteo_Discharge/AnalyseTest_0610/Precipitation")
-args = parser.parse_args()
+# Read the CSV file
+data = pd.read_csv('C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/Meteo_Discharge/AnalyseTest_0610/Discharge/top_20_days_DischargeQStat.csv')
 
-count = 0
+# Convert the 'Date' column to datetime format
+data['Date'] = pd.to_datetime(data['Date'])
 
-# Create an empty DataFrame to store the daily sum values
-daily_sum_df = pd.DataFrame(columns=['Date', 'Sum'])
+# Sort the DataFrame by the 'Date' column
+data.sort_values(by='Date', inplace=True)
 
-# Create an empty DataFrame to store the hourly sum values
-hourly_sum_df = pd.DataFrame(columns=['Date', 'Sum'])
+# Select the top 100 daily mean discharge values
+top_100_data = data.head(100)
 
-# Loop through all the files in the folder
-for file in os.listdir(args.dirin):
-    # Check if the file is a CSV file
-    if file.endswith(".csv"):
-        # Increment the counter variable
-        count += 1
+# Create subplots with adjusted spacing
+fig, ax = plt.subplots(figsize=(10, 6))
+plt.plot(top_100_data['Date'], top_100_data['Sum'], marker='o', linestyle='-', color='b')
+plt.title('Top 100 Daily Mean Discharge Values')
+plt.xlabel('Date')
+plt.ylabel('Mean Discharge m3/s')
+plt.grid(True)
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.legend()
 
-        # Read the CSV file into a pandas DataFrame
-        df = pd.read_csv(os.path.join(args.dirin, file), delimiter='\t')
-        # Extract the base name of the input file without the extension
-        input_file_name = os.path.splitext(os.path.basename(file))[0]
+# Add a grid to the subplot
+ax.grid(True)
 
-
-        # Combine the datetime columns into a single datetime object
-        timestamp = [pd.Timestamp(int(row['YY']), int(row['MM']), int(row['DD']), int(row['HH']), int(row['MN'])) for i, row in df.iterrows()]
-
-        # Convert the timestamp list to a DatetimeIndex object and set it as the new index
-        df.index = pd.DatetimeIndex(timestamp)
-
-        # Convert the DataFrame values to float
-        df['Stat1'] = pd.to_numeric(df['Stat1'], errors='coerce')
-
-        # Calculate the daily sum for the period between April 20th and October 20th each year (summer months)
-        df_filtered = df[(df.index.month >= 4) & (df.index.month <= 10) | ((df.index.month == 4) & (df.index.day >= 20)) | ((df.index.month == 10) & (df.index.day <= 20))]
-        daily_sum = df_filtered['Stat1'].resample('D').sum()
-
-
-        # Create a DataFrame for daily_sum with 'Date' and 'Sum' columns
-        daily_sum = daily_sum.reset_index()
-        daily_sum.columns = ['Date', 'Sum']
-
-        # Append the daily_sum to the daily_sum_df
-        daily_sum_df = pd.concat([daily_sum_df, daily_sum], axis=0)
-
-        # Calculate the hourly sum for the period between April 20th and October 20th each year
-        hourly_sum = df_filtered['Stat1'].resample('H').sum()
-
-        # Create a DataFrame for hourly_sum with 'Date' and 'Sum' columns
-        hourly_sum = hourly_sum.reset_index()
-        hourly_sum.columns = ['Date', 'Sum']
-
-        # Append the hourly_sum to the hourly_sum_df
-        hourly_sum_df = pd.concat([hourly_sum_df, hourly_sum], axis=0)
-
-
-# Sort the daily_sum_df by the 'Sum' column in descending order
-daily_sum_df_sorted = daily_sum_df.sort_values(by='Sum', ascending=False)
-
-# Get the top 20 days with the highest sum values
-top_20_days = daily_sum_df_sorted.head(20)
-
-top_20_days['Sum'] = top_20_days['Sum'].round(2)
-
-# Print the top 20 days
-print("Top 20 days with the highest sum values:")
-print(top_20_days)
-
-# Save the top 20 days to a new CSV file without index and in a cleaner format
-output_csv_path = os.path.join(args.dirout, f"top_20_days_{input_file_name}.txt")
-top_20_days.to_csv(output_csv_path, index=False, header=['Date', 'Sum'])
-print(f"Top 20 days saved to {output_csv_path}")
-
-
-# Sort the hourly_sum_df by the 'Sum' column in descending order
-hourly_sum_df_sorted = hourly_sum_df.sort_values(by='Sum', ascending=False)
-
-# Get the top 20 hours with the highest sum values
-top_20_hours = hourly_sum_df_sorted.head(20)
-
-# Round the 'Sum' values to 2 decimal places
-top_20_hours['Sum'] = top_20_hours['Sum'].round(2)
-
-# Save the top 20 hours to a new CSV file without index and header
-output_top_20_hours_path = os.path.join(args.dirout, f"top_20_hours_{input_file_name}.txt")
-if not top_20_hours.empty:
-    top_20_hours.to_csv(output_top_20_hours_path, index=False, header=['Date', 'Sum'])
-    print(f"Top 20 hours with the highest sum values saved to {output_top_20_hours_path}")
-else:
-    print("No data available for the specified period.")
-
-
-
-
-## Filter periods of rainfall for at least 3 days in a row
-rain_periods = []
-current_rain_period = []
-current_rain_sum = 0  # Variable to keep track of the current rain period's total sum
-for _, row in daily_sum_df.iterrows():
-    if row['Sum'] > 0:
-        current_rain_period.append(row['Date'])
-        current_rain_sum += row['Sum']
-    else:
-        if len(current_rain_period) >= 3:
-            current_rain_sum = round(current_rain_sum, 2)
-            rain_periods.append({'Period': current_rain_period, 'Total Rainfall': current_rain_sum})
-        current_rain_period = []
-        current_rain_sum = 0
-
-# Save the rain periods to a file
-rain_periods_output_path = os.path.join(args.dirout, "rain_periods.txt")
-with open(rain_periods_output_path, 'w') as f:
-    for i, period in enumerate(rain_periods, start=1):
-        f.write(f"Rain Period {i}:\n")
-        f.write(f"Start Date: {period['Period'][0]}\n")
-        f.write(f"End Date: {period['Period'][-1]}\n")
-        f.write(f"Total Rainfall: {period['Total Rainfall']} mm\n\n")
-print(f"Rain periods saved to {rain_periods_output_path}")
-
-
-# Convert the 'Date' columns in both DataFrames to a common date format
-top_20_days['Date'] = top_20_days['Date'].dt.strftime('%Y-%m-%d')
-top_20_hours['Date'] = top_20_hours['Date'].dt.strftime('%Y-%m-%d')
-
-# Check if any of the dates in top_20_days match any of the dates in top_20_hours
-matching_dates = top_20_days['Date'].isin(top_20_hours['Date'])
-
-# Get the matching days
-matching_days = top_20_days[matching_dates]
-
-# Save the matching days to a CSV file
-output_matching_days_path = os.path.join(args.dirout, "matching_top_days.txt")
-
-if not matching_days.empty:
-    matching_days.to_csv(output_matching_days_path, index=False)
-    print(f"Matching days saved to {output_matching_days_path}")
-else:
-    print("No matching days found between top 20 days and top 20 hours, so no file was saved.")
+# Show the plot
+plt.show()
