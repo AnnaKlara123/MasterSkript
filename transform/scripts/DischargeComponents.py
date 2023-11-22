ea94411a1cd2031504e0aa2506fdeed70c3ccf9a
Weltenbummlerin123 <anna.stang@student.uibk.ac.at>
2023-11-22 15:47:46 +0100

    Pie Diagramm und colourd Balken fertig!

--- transform/scripts/DischargeComponents.py
+++ transform/scripts/DischargeComponents.py
@@ -11,43 +11,81 @@ df = pd.read_csv(file_path, delimiter=';')
 # Sort the DataFrame by "Daily max discharge per day" in descending order
 df_sorted = df.sort_values(by='Daily max discharge per day', ascending=False)
 
+# Check the column names in your DataFrame
+print(df_sorted.columns)
+
 # Define a color map for the "Category" column
 colors = {
-    1: 'r',  # Red
-    2: 'g',  # Green
-    3: 'b',  # Blue
-    4: 'y',  # Yellow
-    5: 'c',  # Cyan
-    6: 'm',  # Magenta
+    1: 'lightblue',  # blue = Snowmelt
+    2: 'darkblue',  # Green = SMPrecipitation
+    3: 'red',  # Blue = Icemelt
+    4: 'darkred',  # Yellow = IcemeltPrecipitation
+    5: 'orange',  # Cyan = SMIM Snow and Icemelt
+    6: 'grey',  # Magenta = All together 
 }
 
-# # Create a list of colors based on the "Category" column
-# bar_colors = [colors.get(cat, 'k') for cat in df_sorted['Category']]
-
-# Create a bar chart
+# Create a bar chart with color-coded bars
 plt.figure(figsize=(12, 6))
-plt.bar(np.arange(len(df_sorted)), df_sorted['Daily max discharge per day'])
+
+# Get the colors for each bar based on the numeric "Category" column
+bar_colors = [colors[cat] for cat in df_sorted['Category']]
+
+bars = plt.bar(np.arange(len(df_sorted)), df_sorted['Daily max discharge per day'], color=bar_colors)
 
 # Label the x-axis
-plt.xlabel('Sorted Days')
+plt.xlabel('100 top event days')
 plt.xticks(np.arange(len(df_sorted)), df_sorted.index)
 
 # Label the y-axis
-plt.ylabel('Daily Max Discharge per Day')
-
-# # Add a legend for the categories
-# legend_labels = {1: 'Category 1', 2: 'Category 2', 3: 'Category 3', 4: 'Category 4', 5: 'Category 5', 6: 'Category 6'}
-# legend_handles = [plt.Line2D([0], [0], color=color, lw=4, label=legend_labels[cat]) for cat, color in colors.items()]
-# plt.legend(handles=legend_handles, title='Categories')
+plt.ylabel('Max discharge value per Day m3/s')
 
 # Set the title
-plt.title('Daily Max Discharge per Day Sorted by Category')
+plt.title('Sorted daily max discharge per Day in categories')
+
+# Create a legend
+legend_labels = {
+    1: 'Snowmelt',
+    2: 'SMPrecipitation',
+    3: 'Icemelt',
+    4: 'IcemeltPrecipitation',
+    5: 'Snow and Icemelt',
+    6: 'All together',
+}
+legend_handles = [plt.Rectangle((0, 0), 1, 1, color=colors[cat]) for cat in legend_labels.keys()]
+plt.legend(legend_handles, legend_labels.values(), title='Factor')
 
 # Show the plot
 plt.tight_layout()
 plt.show()
 
+############ PIE Diagramm ##################
+# Group the data by "Category" and calculate the percentage of each category
 
-print("done")
+category_counts = df['Category'].value_counts()
+total_count = len(df)
+category_percentages = (category_counts / total_count) * 100
 
-# Now, you can access and work with your data using the 'df' DataFrame
+# Define colors for the pie chart
+colors = ['lightblue', 'darkblue', 'red', 'darkred', 'orange', 'grey']
+
+# Create a pie chart
+plt.figure(figsize=(8, 8))
+plt.pie(category_percentages, labels=None, autopct='%1.1f%%', colors=colors, startangle=140)
+plt.title('Factors Contributing to High Flood Events')
+
+# Create a legend
+legend_labels = {
+    1: 'Snowmelt',
+    2: 'SMPrecipitation',
+    3: 'Icemelt',
+    4: 'IcemeltPrecipitation',
+    5: 'Snow and Icemelt',
+    6: 'All together',
+}
+
+legend_handles = [plt.Rectangle((0, 0), 1, 1, color=colors[cat - 1]) for cat in legend_labels.keys()]
+plt.legend(legend_handles, legend_labels.values(), title='Factor', loc='center left', bbox_to_anchor=(1, 0.5))
+# Show the plot
+plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+plt.tight_layout()
+plt.show()
