34e0eec85597aa1e2eaea1e03a005489db974525
Weltenbummlerin123 <anna.stang@student.uibk.ac.at>
2023-11-26 16:12:41 +0100

    Anapssen der y-axe für alle Datensätze!!

--- transform/scripts/plot_data_Specific_day.py
+++ transform/scripts/plot_data_Specific_day.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.dates as mdates
 
 # Define the path to the folder containing the .csv files
-folder_path_Discharge = 'C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/Meteo_Discharge/Filtered'
+folder_path_Discharge = 'C:/Users/annak/OneDrive/Documents/Master/Masterarbeit/Meteo_Discharge/Filtered_OHNEGH'
 
 # Get a list of all .csv files in the folder
 file_list_Discharge = [file for file in os.listdir(folder_path_Discharge) if file.endswith(".csv")]
@@ -16,6 +16,16 @@ file_list_Discharge = [file for file in os.listdir(folder_path_Discharge) if fil
 timestamps = []
 column_names_Discharge = []
 data_Discharge = {}
+
+# Define the specific minimum and maximum values for each dataset
+y_limits = [
+    (0, 18),  # Dataset 1
+    (0, 10),  # Dataset 2
+    (0, 1300),  # Dataset 3
+    (-25, 30),  # Dataset 4
+    (0, 101)   # Dataset 5
+]
+
 
 # Loop through each file and read in the data
 for file_name in file_list_Discharge:
@@ -34,15 +44,15 @@ for file_name in file_list_Discharge:
     timestamps.append(timestamp_Discharge)
 
 # Define the central date and the date range around it
-central_date = pd.Timestamp('2018-06-15')
-start_date = central_date - pd.Timedelta(days=10)
-end_date = central_date + pd.Timedelta(days=90) # bei Jahr 80
+central_date = pd.Timestamp('2021-01-02')
+start_date = central_date - pd.Timedelta(days=580)
+end_date = central_date + pd.Timedelta(days=600) # bei Jahr 80
 
 # Calculate the number of datasets based on the length of column_names_Discharge
 num_datasets = len(column_names_Discharge)
 # Define a list of colors to cycle through
-colors = cycle(['b', 'g', 'm', 'c', 'c', 'k', 'k'])
-#colors = cycle(['b', 'g', 'm', 'c', 'k'])
+#colors = cycle(['b', 'g', 'm', 'c', 'c', 'k', 'k'])
+colors = cycle(['b', 'g', 'm', 'c', 'k'])
 
 
 # Define the labels for each dataset
@@ -52,6 +62,44 @@ labels = ["Discharge m³/s", "Precipitation mm/m²", "Global Radiation W/m²", "
 fig, axs = plt.subplots(num_datasets, 1, figsize=(10, 6*num_datasets), sharex=True, 
                         gridspec_kw={'top': 0.85, 'bottom': 0.11, 'left': 0.45, 'right': 0.9, 'hspace': 0.10, 'wspace': 0.10},
                         constrained_layout=True)
+
+# # Plot each dataset on a separate subplot with a different color
+# for i, column in enumerate(column_names_Discharge):
+#     # Filter the timestamps and data based on the date range
+#     mask = (np.array(timestamps[i]) >= start_date) & (np.array(timestamps[i]) <= end_date)
+#     filtered_timestamps = np.array(timestamps[i])[mask]
+#     filtered_data = np.array(data_Discharge[column])[mask]
+    
+#     # Create arrays for x and y values without NaN
+#     x_values = []
+#     y_values = []
+#     for x, y in zip(filtered_timestamps, filtered_data):
+#         if not np.isnan(y):
+#             x_values.append(x)
+#             y_values.append(y)
+
+#     axs[i].plot(x_values, y_values, color=next(colors), label=column, linewidth=0.5)
+
+#     # Set the x-axis limits to the specified date range
+#     axs[i].set_xlim(start_date, end_date)
+
+#     # Find the index of the maximum value in the current dataset
+#     max_index = np.argmax(y_values)
+
+#     # Get the timestamp and value at the peak
+#     peak_timestamp = x_values[max_index]
+#     peak_value = y_values[max_index]
+
+#     # Set the y-axis limits for each subplot individually
+#     y_min = min(y_values) - 0.5
+#     y_max = max(y_values) + 0.5
+#     axs[i].set_ylim(y_min, y_max)
+
+#     # Add a grid to the subplot
+#     axs[i].grid(True)
+
+#     # Add labels to the subplots
+#     axs[i].set_ylabel(labels[i])
 
 # Plot each dataset on a separate subplot with a different color
 for i, column in enumerate(column_names_Discharge):
@@ -73,17 +121,16 @@ for i, column in enumerate(column_names_Discharge):
     # Set the x-axis limits to the specified date range
     axs[i].set_xlim(start_date, end_date)
 
+    # Set the y-axis limits using the specific values for the dataset
+    y_min, y_max = y_limits[i]
+    axs[i].set_ylim(y_min, y_max)  # Use specific min and max values
+
     # Find the index of the maximum value in the current dataset
     max_index = np.argmax(y_values)
 
     # Get the timestamp and value at the peak
     peak_timestamp = x_values[max_index]
     peak_value = y_values[max_index]
-
-    # Set the y-axis limits for each subplot individually
-    y_min = min(y_values) - 0.5
-    y_max = max(y_values) + 0.5
-    axs[i].set_ylim(y_min, y_max)
 
     # Add a grid to the subplot
     axs[i].grid(True)
